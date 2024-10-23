0439ac120f69aaadb5372937b3890acf73ed6241
Takmim00 <takmimm@gmail.com>
2024-10-23 20:57:47 +0600

    btn functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Recipes from "./Componenets/Recipes";
 import Sidebar from "./Componenets/Sidebar";
 
 function App() {
-  const [recipeQueue, setRecipeQueue] = useState([])
+  const [recipeQueue, setRecipeQueue] = useState([]);
 
-  const addRecipeToQueue = recipe =>{
-    setRecipeQueue ([...recipeQueue,recipe])
-  }
+  const addRecipeToQueue = (recipe) => {
+    const isExist = recipeQueue.find(
+      (previousRecipe) => previousRecipe.recipe_id === recipe.recipe_id
+    );
+    if (!isExist) {
+      setRecipeQueue([...recipeQueue, recipe]);
+    } else {
+      alert("Recipes already exists in the queue.");
+    }
+  };
   console.log(recipeQueue);
   return (
     <div className="w-11/12 mx-auto">
-    <Header></Header>
-    <OurRecipes></OurRecipes>
+      <Header></Header>
+      <OurRecipes></OurRecipes>
       <section className="flex flex-col md:flex-row gap-6">
         <Recipes addRecipeToQueue={addRecipeToQueue}></Recipes>
         <Sidebar></Sidebar>
